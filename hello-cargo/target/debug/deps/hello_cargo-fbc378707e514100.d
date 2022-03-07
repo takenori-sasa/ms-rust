fe27ba20875c16fcaa04f32c65235736db8756eb
takenori-sasa <yoshida.tkw@gmail.com>
2022-03-07 18:08:45 +0900

    hello-cargo

---
+++ hello-cargo/target/debug/deps/hello_cargo-fbc378707e514100.d
@@ -0,0 +1,5 @@
+/Users/takenori/ghq/github.com/takenori-sasa/ms-rust/hello-cargo/target/debug/deps/hello_cargo-fbc378707e514100: src/main.rs
+
+/Users/takenori/ghq/github.com/takenori-sasa/ms-rust/hello-cargo/target/debug/deps/hello_cargo-fbc378707e514100.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/Users/takenori/ghq/github.com/takenori-sasa/ms-rust/hello-cargo/target/debug/hello-cargo: /Users/takenori/ghq/github.com/takenori-sasa/ms-rust/hello-cargo/src/main.rs
