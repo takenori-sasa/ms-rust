0e16d404ee236609d50c7ee43d7f67d60ce71be8
takenori-sasa <yoshida.tkw@gmail.com>
2022-03-08 16:22:27 +0900

    loopした

--- for-loop/src/main.rs
+++ for-loop/src/main.rs
@@ -41,9 +41,9 @@ fn car_factory(order: i32, miles: u32) -> Car {
     // Valid color = 1, 2, 3, or 4
     // If color > 4, reduce color to valid index
     let mut color = order as usize;
-    if color > 4 {
+    while color > 4 {
         // color = 5 --> index 1, 6 --> 2, 7 --> 3, 8 --> 4
-        color = color - 4;
+        color -= 4;
     }
 
     // Add variety to orders for motor type and roof type
@@ -71,42 +71,24 @@ fn main() {
     use std::collections::HashMap;
     let mut orders: HashMap<i32, Car> = HashMap::new();
     // Initialize counter variable
-    let mut order = 1;
     // Declare a car as mutable "Car" struct
     let mut car: Car;
-
-    // Order 6 cars, increment "order" for each request
-    // Car order #1: Used, Hard top
-    car = car_factory(order, 1000);
-    orders.insert(order, car);
-    println!("Car order {}: {:?}", order, orders.get(&order));
-
-    // Car order #2: Used, Convertible
-    order = order + 1;
-    car = car_factory(order, 2000);
-    orders.insert(order, car);
-    println!("Car order {}: {:?}", order, orders.get(&order));
-    // Car order #3: New, Hard top
-    order = order + 1;
-    car = car_factory(order, 0);
-    orders.insert(order, car);
-    println!("Car order {}: {:?}", order, orders.get(&order));
-
-    // Car order #4: New, Convertible
-    order = order + 1;
-    car = car_factory(order, 0);
-    orders.insert(order, car);
-    println!("Car order {}: {:?}", order, orders.get(&order));
-
-    // Car order #5: Used, Hard top
-    order = order + 1;
-    car = car_factory(order, 3000);
-    orders.insert(order, car);
-    println!("Car order {}: {:?}", order, orders.get(&order));
-
-    // Car order #6: Used, Hard top
-    order = order + 1;
-    car = car_factory(order, 4000);
-    orders.insert(order, car);
-    println!("Car order {}: {:?}", order, orders.get(&order));
+    let mut miles = 0;
+    let mut order = 1;
+    loop {
+        // Order 6 cars, increment "order" for each request
+        car = car_factory(order, miles);
+        orders.insert(order, car);
+        println!("Car order {}: {:?}", order, orders.get(&order));
+        // Reset miles for order variety
+        if miles == 2100 {
+            miles = 0;
+        } else {
+            miles = miles + 700;
+        }
+        order += 1;
+        if order > 11 {
+            break;
+        }
+    }
 }
